b6aae4c67b526e2d95ad58ef2b90e7096481a7d1
Fabio Somaglia <fabio.somaglia@gmail.com>
2025-08-28 10:50:41 +0300

    feat(docs): add installation instructions and improve examples for stateful button

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { ModeToggle } from '@/components/mode-toggle';
 import StatefulButton from '@/registry/new-york/ui/stateful-button/stateful-button';
 import useProgress from '@/hooks/use-progress';
 import { ComponentPreview } from '@/components/component-preview';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import CodeBlock from '@/components/code-block';
+import { Terminal } from 'lucide-react';
 
 export default function Home() {
 	const { progress, startProgress } = useProgress();
@@ -35,11 +38,10 @@ export default function Home() {
 						for providing feedback to the user after an action has been performed.
 					</p>
 				</header>
-				<main className="flex flex-1 flex-col gap-8">
+				<main className="flex flex-1 flex-col gap-6">
 					<ComponentPreview
 						preview={
 							<StatefulButton
-								buttonType="spinner"
 								onClick={async () => {
 									console.log('clicked');
 									await loadingSuccessTest();
@@ -50,8 +52,16 @@ export default function Home() {
 								Load
 							</StatefulButton>
 						}
-						code={`<StatefulButton
-  buttonType="spinner"
+						code={`import StatefulButton from '@/components/ui/stateful-button';
+
+// dummy success API call
+const loadingSuccessTest = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 4000);
+  });
+};
+
+<StatefulButton
   onClick={async () => {
     console.log('clicked');
     await loadingSuccessTest();
@@ -63,26 +73,8 @@ export default function Home() {
 </StatefulButton>`}
 					/>
 
-					<div className="relative flex min-h-[450px] flex-col gap-4 rounded-lg border p-4">
-						<h2 className="text-sm text-muted-foreground sm:pl-3">Stateful Button with loading and error state</h2>
-						<div className="relative flex min-h-[400px] items-center justify-center">
-							<StatefulButton
-								buttonType="spinner"
-								onClick={async () => {
-									console.log('clicked');
-									await loadingFailedTest();
-								}}
-								onComplete={() => console.log('completed')}
-								onError={(error) => console.error(error)}
-							>
-								Load
-							</StatefulButton>
-						</div>
-					</div>
-
-					<div className="relative flex min-h-[450px] flex-col gap-4 rounded-lg border p-4">
-						<h2 className="text-sm text-muted-foreground sm:pl-3">Stateful Button with progress and success state</h2>
-						<div className="relative flex min-h-[400px] items-center justify-center">
+					<ComponentPreview
+						preview={
 							<StatefulButton
 								buttonType="progress"
 								onClick={async () => {
@@ -95,26 +87,195 @@ export default function Home() {
 							>
 								Progress
 							</StatefulButton>
-						</div>
-					</div>
+						}
+						code={`import StatefulButton from '@/components/ui/stateful-button';
 
-					<div className="relative flex min-h-[450px] flex-col gap-4 rounded-lg border p-4">
-						<h2 className="text-sm text-muted-foreground sm:pl-3">Stateful Button with progress and error state</h2>
-						<div className="relative flex min-h-[400px] items-center justify-center">
+<StatefulButton
+  buttonType="progress"
+  onClick={async () => {
+    console.log('clicked');
+    await startProgress(3, 100);
+  }}
+  onComplete={() => console.log('completed')}
+  onError={(error) => console.error(error)}
+  progress={progress}
+>
+  Progress
+</StatefulButton>`}
+					/>
+
+					<h2 className="mt-12 text-2xl font-medium tracking-tight">Installation</h2>
+					<Tabs defaultValue="npm">
+						<TabsList>
+							<TabsTrigger value="icon" disabled>
+								<div className="rounded-[1px] bg-foreground text-background">
+									<Terminal className="m-0.5 size-3.5" />
+								</div>
+							</TabsTrigger>
+							<TabsTrigger value="pnpm">pnpm</TabsTrigger>
+							<TabsTrigger value="npm">npm</TabsTrigger>
+							<TabsTrigger value="yarn">yarn</TabsTrigger>
+							<TabsTrigger value="bun">bun</TabsTrigger>
+						</TabsList>
+						<TabsContent value="pnpm">
+							<CodeBlock lang="bash" textSize={14} showLineNumbers={false}>
+								{`pnpm dlx shadcn@latest add http://localhost:3000/r/stateful-button.json`}
+							</CodeBlock>
+						</TabsContent>
+						<TabsContent value="npm">
+							<CodeBlock lang="bash" textSize={14} showLineNumbers={false}>
+								{`npx shadcn@latest add http://localhost:3000/r/stateful-button.json`}
+							</CodeBlock>
+						</TabsContent>
+						<TabsContent value="yarn">
+							<CodeBlock lang="bash" textSize={14} showLineNumbers={false}>
+								{`yarn shadcn@latest add http://localhost:3000/r/stateful-button.json`}
+							</CodeBlock>
+						</TabsContent>
+						<TabsContent value="bun">
+							<CodeBlock lang="bash" textSize={14} showLineNumbers={false}>
+								{`bunx --bun shadcn@latest add http://localhost:3000/r/stateful-button.json`}
+							</CodeBlock>
+						</TabsContent>
+					</Tabs>
+
+					<h2 className="mt-12 text-2xl font-medium tracking-tight">Examples</h2>
+					<h3 className="mt-4 text-xl font-semibold tracking-tight">Default</h3>
+					<p className="leading-relaxed">
+						The stateful button automatically handles success and error states based on the outcome of the{' '}
+						<code className="relative rounded-md bg-muted px-[0.3rem] py-[0.2rem] font-mono text-[0.8rem] outline-none">
+							onClick
+						</code>{' '}
+						event. If the promise resolves, the button enters a success state; if it rejects, it enters an error state.
+						The "Default" variant examples below demonstrate both scenarios.
+					</p>
+
+					<ComponentPreview
+						preview={
+							<StatefulButton
+								onClick={loadingSuccessTest}
+								onComplete={() => console.log('completed')}
+								onError={(error) => console.error(error)}
+							>
+								Load
+							</StatefulButton>
+						}
+						code={`import StatefulButton from '@/components/ui/stateful-button';
+
+// dummy success API call
+const loadingSuccessTest = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 4000);
+  });
+};
+
+<StatefulButton
+  onClick={async () => {
+    console.log('clicked');
+    await loadingSuccessTest();
+  }}
+  onComplete={() => console.log('completed')}
+  onError={(error) => console.error(error)}
+>
+  Load
+</StatefulButton>`}
+					/>
+
+					<ComponentPreview
+						preview={
+							<StatefulButton
+								onClick={loadingFailedTest}
+								onComplete={() => console.log('completed')}
+								onError={(error) => console.error(error)}
+								className="w-32"
+							>
+								Load with error
+							</StatefulButton>
+						}
+						code={`import StatefulButton from '@/components/ui/stateful-button';
+
+// dummy failed API call
+const loadingFailedTest = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(new Error('Error from dummy API call')), 4000);
+  });
+};
+
+<StatefulButton
+  onClick={loadingFailedTest}
+  onComplete={() => console.log('completed')}
+  onError={(error) => console.error(error)}
+  className="w-32"
+>
+  Load with error
+</StatefulButton>`}
+					/>
+
+					<ComponentPreview
+						preview={
 							<StatefulButton
 								buttonType="progress"
+								progress={progress}
 								onClick={async () => {
 									console.log('clicked');
-									await startProgress(3, 100, () => Math.random() < 0.05);
+									await startProgress(3, 100);
 								}}
 								onComplete={() => console.log('completed')}
 								onError={(error) => console.error(error)}
-								progress={progress}
 							>
 								Progress
 							</StatefulButton>
-						</div>
-					</div>
+						}
+						code={`import StatefulButton from '@/components/ui/stateful-button';
+
+<StatefulButton
+  buttonType="progress"
+  progress={progress}
+  onClick={async () => {
+    console.log('clicked');
+    await startProgress(3, 100);
+  }}
+  onComplete={() => console.log('completed')}
+  onError={(error) => console.error(error)}
+>
+  Progress
+</StatefulButton>`}
+					/>
+
+					<ComponentPreview
+						preview={
+							<StatefulButton
+								buttonType="progress"
+								progress={progress}
+								onClick={async () => {
+									console.log('clicked');
+									await startProgress(3, 100, (val) => val >= 75, 'Failed at 75%');
+								}}
+								onComplete={() => console.log('completed')}
+								onError={(error) => console.error(error)}
+								className="w-38"
+							>
+								Progress with error
+							</StatefulButton>
+						}
+						code={`import StatefulButton from '@/components/ui/stateful-button';
+
+<StatefulButton
+  buttonType="progress"
+  progress={progress}
+  onClick={async () => {
+    console.log('clicked');
+    await startProgress(3, 100, (val) => val >= 75, 'Failed at 75%');
+  }}
+  onComplete={() => console.log('completed')}
+  onError={(error) => console.error(error)}
+  className="w-38"
+>
+  Progress with error
+</StatefulButton>`}
+					/>
+
+					<h3 className="mt-4 text-xl font-semibold tracking-tight">Secondary</h3>
 
 					<div className="relative flex min-h-[450px] flex-col gap-4 rounded-lg border p-4">
 						<h2 className="text-sm text-muted-foreground sm:pl-3">
