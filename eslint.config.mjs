b2cb1e02021bd8480b8c02afd97b0d2f75b0cfcc
Fabio Somaglia <fabio.somaglia@gmail.com>
2026-01-04 14:39:58 +0100

    build: set version to 3.0.0, bump dependencies, upgrade Next.js to v16 and correct CVE-2025-55182

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -10,7 +12,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-	...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+	...nextCoreWebVitals,
+	...nextTypescript,
+	...compat.extends('prettier'),
 	{
 		ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts']
 	}
